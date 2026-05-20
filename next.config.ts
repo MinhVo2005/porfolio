13f8aaf60c205cd958e699f2b641cc19ca3a3026
Minh Vo <minh.vo2@mail.mcgill.ca>
2026-05-19 20:06:17 -0400

    fix dockerfile

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  outputFileTracingIncludes: {
+    "/**": ["./node_modules/figlet/fonts/**"],
+  },
 };
 
 export default nextConfig;
